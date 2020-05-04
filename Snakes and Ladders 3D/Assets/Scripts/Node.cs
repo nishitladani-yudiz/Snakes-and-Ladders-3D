4ce3b6356179a9a49a27b0db0565c46d28f28397
nishitladani-yudiz <63995647+nishitladani-yudiz@users.noreply.github.com>
2020-05-04 18:09:46 +0530

    Made the game's board

---
+++ Snakes and Ladders 3D/Assets/Scripts/Node.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour
+{
+    public int nodeID;              // Position in the route
+    public Text numberText;
+    public Node connectedNode;      // Ladder or Snake
+
+
+
+    public void SetNodeID(int _nodeID)
+    {
+        nodeID = _nodeID;
+        if(numberText != null)
+        {
+            numberText.text = nodeID.ToString();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if(connectedNode != null)
+        {
+            Color col = Color.white;
+
+            col = (connectedNode.nodeID > nodeID) ? Color.blue : Color.red;
+            Debug.DrawLine(transform.position, connectedNode.transform.position, col);
+        }
+    }
+}
---
+++ Snakes and Ladders 3D/Assets/Scripts/Route.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Route : MonoBehaviour
+{
+    Transform[] nodes;
+    List<Transform> nodeList = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        FillNodes();
+    }
+
+    void FillNodes()
+    {
+        nodeList.Clear();
+        nodes = GetComponentsInChildren<Transform>();
+
+        int num = -1;
+        foreach(Transform child in nodes)
+        {
+            Node n = child.GetComponent<Node>();
+            if(child != this.transform && n != null)
+            {
+                num++;
+                nodeList.Add(child);
+                child.gameObject.name = "field " + num;
+                
+                // Fill in node id to the node
+
+                n.SetNodeID(num);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        FillNodes();
+        
+        for(int i = 0; i < nodeList.Count; i++)
+        {
+            Vector3 start = nodeList[i].position;
+            if(i > 0)
+            {
+                Vector3 previous = nodeList[i - 1].position;
+                Debug.DrawLine(previous, start, Color.green);
+            }
+        }
+    }
+
+}
